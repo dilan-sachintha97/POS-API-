57814b1ebff2fee2a41011b4476f9a245446f7c8
dilan-sachintha97 <dil97an@gmail.com>
2022-11-20 14:10:27 +0530

    init the project

---
+++ src/main/java/com/batch80/pointofsalebatch80/PointOfSaleBatch80Application.java
@@ -0,0 +1,13 @@
+package com.batch80.pointofsalebatch80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSaleBatch80Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSaleBatch80Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch80/pointofsalebatch80/PointOfSaleBatch80ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch80.pointofsalebatch80;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleBatch80ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
