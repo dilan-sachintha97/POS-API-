a35e6efd1b61e0bc778b24e6625fc0db7b87a499
dilan-sachintha97 <dil97an@gmail.com>
2022-11-20 16:49:50 +0530

    done customer save method

---
+++ src/main/java/com/batch80/pointofsalebatch80/repo/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.batch80.pointofsalebatch80.repo;
+
+import com.batch80.pointofsalebatch80.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+@EnableJpaRepositories
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+}
--- src/main/java/com/batch80/pointofsalebatch80/service/impl/CustomerServiceIML.java
+++ src/main/java/com/batch80/pointofsalebatch80/service/impl/CustomerServiceIML.java
@@ -1,13 +1,35 @@
 package com.batch80.pointofsalebatch80.service.impl;
 
 import com.batch80.pointofsalebatch80.dto.CustomerDTO;
+import com.batch80.pointofsalebatch80.entity.Customer;
+import com.batch80.pointofsalebatch80.repo.CustomerRepo;
 import com.batch80.pointofsalebatch80.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceIML implements CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
+
     @Override
     public String addCustomer(CustomerDTO customerDTO) {
-        return null;
+        Customer customer = new Customer(
+                customerDTO.getCustomerId(),
+                customerDTO.getCustomerName(),
+                customerDTO.getCustomerAddress(),
+                customerDTO.getCustomerSalary(),
+                customerDTO.getContactNumber(),
+                customerDTO.getNic(),
+                customerDTO.isActiveState()
+        );
+        if(!customerRepo.existsById(customer.getCustomerId())){
+            customerRepo.save(customer);
+            return "Saved Customer !";
+        }else {
+            return "Customer ID already exists !";
+        }
+
     }
 }
