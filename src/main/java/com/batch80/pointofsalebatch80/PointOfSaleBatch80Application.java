7c9b24d094bb2c4734865ab515914ec83d58d766
dilan-sachintha97 <dil97an@gmail.com>
2022-11-21 13:35:38 +0530

    add swagger configuration

--- src/main/java/com/batch80/pointofsalebatch80/PointOfSaleBatch80Application.java
+++ src/main/java/com/batch80/pointofsalebatch80/PointOfSaleBatch80Application.java
@@ -2,8 +2,10 @@ package com.batch80.pointofsalebatch80;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class PointOfSaleBatch80Application {
 
 	public static void main(String[] args) {
